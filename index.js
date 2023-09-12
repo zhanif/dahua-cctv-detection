a7b2b05e2ff409d7ea2f79dbef5cf82ec0ca484c
zhanif <ihsanfauzan76@gmail.com>
2023-09-12 22:16:20 +0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const ipcamera = require('./dahua')
+
+const options = {
+  host: process.env.DAHUA_HOST,
+  port: process.env.DAHUA_PORT,
+  user: process.env.DAHUA_USERNAME,
+  pass: process.env.DAHUA_PASSWORD,
+  log: process.env.DAHUA_LOG
+}
+
+const dahua = new ipcamera.dahua(options)
+
+dahua.on('connect', () => {
+  console.log('connected!')
+})
+
+dahua.on('error', (error) => {
+  console.log(error)
+})
